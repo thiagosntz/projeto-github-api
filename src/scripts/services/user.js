146eed5e5b208b67f9ac941b2f62e24f602de1c5
Thiago <thiagosantosjr11@gmail.com>
2022-12-29 00:55:03 -0300

    teste3

--- src/scripts/services/user.js
+++ src/scripts/services/user.js
@@ -1,5 +1,3 @@
-// import { baseUrl} from "/src/scripts/variables.js"
-
 // buscando os dados do usuário
 async function getUser(userName){
     const response = await fetch(`https://api.github.com/users/${userName}`)
